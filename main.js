8ccc1bec649efb278e3f08802f1141769a3205e1
dabek782 <kubadabrowski782@gmail.com>
2024-10-11 20:52:55 +0200

    Added function to simplify code and creation of planet

--- main.js
+++ main.js
@@ -31,7 +31,7 @@ const bgTexture = texture_loader.load(stars)
 bgTexture.colorSpace = three.SRGBColorSpace
 scene.background = bgTexture*/
 const texture_loader = new three.TextureLoader();
-const cube_background = new three.CubeTextureLoader()
+/*const cube_background = new three.CubeTextureLoader()
 const texture = cube_background.load([
   space,
   space,
@@ -40,26 +40,26 @@ const texture = cube_background.load([
   space,
   stars
 ])
-scene.background = texture;
+scene.background = texture;*/
 
 
 rendering.setSize(innerWidth , innerHeight);
 rendering.setPixelRatio(devicePixelRatio);
-//camera helper grid helper and axis helper
-//const helper = new three.CameraHelper(camera);
-//scene.add(helper)
+/*camera helper grid helper and axis helper
+const helper = new three.CameraHelper(camera);
+scene.add(helper)
 const axis_helper = new three.AxesHelper(30);
 scene.add(axis_helper);
 const grid_helper = new three.GridHelper(200);
 scene.add(grid_helper);
-
+*/
+rendering.setClearColor(0x0000000)
 //setting up mouse control
 const controls =  new OrbitControls(camera, rendering.domElement)
 camera.position.set(20,20,20);
 controls.update();
-// setting up ambient light from the sun
-const amlight = new three.AmbientLight( 0x404040 );
-scene.add( amlight );
+
+
 
 
 
@@ -73,109 +73,79 @@ const sun_material = new three.MeshBasicMaterial({
 const sun = new three.Mesh(sun_geo,sun_material);
 scene.add(sun);
 sun.position.set(0,0,0)
-const pointlight = new three.PointLight(0xFFFFFFFF,2,30)
+const pointlight = new three.PointLight(0xFFFFFFFF,5,100)
 scene.add(pointlight);
 pointlight.position.set(10,0,0)
-//seting up mercury
-const mercury_geo = new three.SphereGeometry(0.0701308035072589,64,32)
-const mercury_material = new three.MeshStandardMaterial({
-  map: texture_loader.load(Mercury),
-  wireframe : false
-})
-const mercury = new three.Mesh(mercury_geo,mercury_material);
-sun.add(mercury)
-mercury.position.set(10.39,0,0)
-//Venus
-const venus_geo = new three.SphereGeometry(0.1739830386660917,64,32)
-const venus_material = new three.MeshStandardMaterial({
-  map: texture_loader.load(Venus),
-  wireframe : false
-})
-const venus = new three.Mesh(venus_geo,venus_material);
-scene.add(venus)
-venus.position.set(10.72,0,0)
-//Earth
-const earth_geo = new three.SphereGeometry(0.1833548943510134,64,32)
-const earth_material = new three.MeshStandardMaterial({
-  map: texture_loader.load(Earth),
-  wireframe : false
-})
-const earth = new three.Mesh(earth_geo,earth_material);
-scene.add(earth)
-earth.position.set(11.11,0,0)
-//Moon
-const moon_geo = new three.SphereGeometry(0.049946816156389264,32)
-const moon_material = new three.MeshStandardMaterial({
-  map: texture_loader.load(Moon),
-  wireframe : false
-})
-const moon = new three.Mesh(moon_geo,moon_material);
-scene.add(moon)
-moon.position.set(11.11,0,0.25)
 
-//Mars
-const mars_geo = new three.SphereGeometry(0.0869565217391304,64,32)
-const mars_material = new three.MeshStandardMaterial({
-  map: texture_loader.load(Mars),
-  wireframe : false
-})
-const mars = new three.Mesh(mars_geo,mars_material);
-scene.add(mars)
-mars.position.set(11.52,0,0)
-//Jupiter
-const jupiter_geo = new three.SphereGeometry(2.055253701308035,64,32)
-const jupiter_material = new three.MeshStandardMaterial({
-  map: texture_loader.load(Jupiter),
-  wireframe : false
-})
-const jupiter = new three.Mesh(jupiter_geo,jupiter_material);
-scene.add(jupiter)
-jupiter.position.set(15.,0,0)
 
-//Saturn
-const saturn_geo = new three.SphereGeometry(1.732585884720425,64,32)
-const saturn_material = new three.MeshStandardMaterial({
-  map: texture_loader.load(Saturn),
-  wireframe : false
+//seting up function that creates our planets without copying code
+function createPlanet (size , texture , position){
+  const geo = new three.SphereGeometry(size,40,40)
+  const mat = new three.MeshStandardMaterial({
+  map: texture_loader.load(texture)
 })
-const saturn = new three.Mesh(saturn_geo,saturn_material);
-scene.add(saturn)
-saturn.position.set(19.54,0,0)
+const mesh = new three.Mesh(geo,mat);
+const obj = new three.Object3D
+obj.add(mesh)
+scene.add(mesh)
+
+mesh.position.x = position
+
+return{mesh , obj}
+}
+
+const mercury = createPlanet(0.0701308035072589,Mercury,10.39)
+const venus = createPlanet(0.1739830386660917,Venus,10.72)
+const earth= createPlanet(0.1833548943510134,Earth,11.11)
+const moon= createPlanet(0.049946816156389264,Moon,11.40)
+const mars = createPlanet(0.0869565217391304,Mars,11.75)
+const jupiter = createPlanet(2.055253701308035,Jupiter,15)
+const saturn = createPlanet(1.732585884720425,Saturn,19.54)
+const uranus= createPlanet(0.7347707345120023,Uranus,29.2)
+const neptune = createPlanet(0.7119160557711657,Neptune,40.6)
+
+
 //Saturn rings
-const rings_geo = new three.TorusGeometry(1.9,0.05,64,32)
-const rings_material = new three.MeshStandardMaterial({
-  map: texture_loader.load(Saturn_Rings),
-  wireframe : false
+const ring_geo = new three.RingGeometry(2,1.5)
+const ring_material = new three.MeshBasicMaterial({
+  map:texture_loader.load(Saturn_Rings),
+  side:three.DoubleSide
 })
-const rings = new three.Mesh(rings_geo,rings_material);
-scene.add(rings);
-rings.position.set(19.54,0,0);
-rings.rotateX(2.041)
-//Uranus
-const uranus_geo = new three.SphereGeometry(0.7347707345120023,64,32)
-const uranus_material = new three.MeshStandardMaterial({
-  map: texture_loader.load(Uranus),
-  wireframe : false
-})
-const uranus = new three.Mesh(uranus_geo,uranus_material);
-scene.add(uranus)
-uranus.position.set(29.2,0,0)
-//Neptune
-const neptune_geo = new three.SphereGeometry(0.7119160557711657,64,32)
-const neptune_material = new three.MeshStandardMaterial({
-  map: texture_loader.load(Neptune),
-  wireframe : false
-})
-const neptune = new three.Mesh(neptune_geo,neptune_material);
-scene.add(neptune)
-neptune.position.set(40.6,0,0)
+const rings = new three.Mesh(ring_geo , ring_material)
+rings.position.set(19.54,0,0)
+scene.add(rings)
+rings.rotation.x = Math.PI/2
 
-//animation
+
+
+
+
+
+ 
 
 function animate(){
   rendering.setAnimationLoop(animate);
   rendering.render(scene , camera)
-  sun.rotateY(0.01)
+  //self rotation
+  sun.rotateY(0.04)
+  mercury.mesh.rotateY(0.04)
+  mercury.obj.rotateY(0.04)
+  venus.mesh.rotateY(0.04)
+  venus.obj.rotateY(0.04)
+  earth.mesh.rotateY(0.04)
+  earth.obj.rotateY(0.04)
+  moon.mesh.rotateY(0.04)
+  moon.obj.rotateY(0.04)
+  mars.mesh.rotateY(0.04)
+  mars.obj.rotateY(0.04)
+  jupiter.mesh.rotateY(0.04)
+  jupiter.obj.rotateY(0.04)
+  saturn.mesh.rotateY(0.04)
+  saturn.obj.rotateY(0.04)
+  uranus.mesh.rotateY(0.04)
+  uranus.obj.rotateY(0.04)
+  neptune.mesh.rotateY(0.04)
+  neptune.obj.rotateY(0.04)
 
 }
 animate()
